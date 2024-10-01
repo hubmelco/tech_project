248158c4c53ae3b7b7aea445857d428b1a9dd3e8
camrcast <camrcast@gmail.com>
2024-10-01 06:27:16 -0700

    added create token from Foundations project

--- src/repository/loginDAO.js
+++ src/repository/loginDAO.js
@@ -10,3 +10,18 @@ async function login(username, password){
     }
     return 2;
 }
+
+async function createToken(user){
+    const token = jwt.sign({
+            id: user.id,
+            username: user.username,
+            role: user.role,
+            favorites: user.favorites
+        },
+        key,
+        {
+            expiresIn: "50m",
+        }
+    );
+    return token;
+}
