b2a91b9c419048253b87549373e87e9724566dd6
camrcast <camrcast@gmail.com>
2024-10-03 13:38:04 -0700

    rewrote tests to mirror user tests (mostly)

--- __tests__/postService.test.js
+++ __tests__/postService.test.js
@@ -1,22 +1,72 @@
 const { createPost } = require("../src/services/postService");
 const { sendPost } = require("../src/repository/postDAO");
+const uuid = require('uuid');
+
+jest.mock('../src/repository/postDAO');
+const mockDatabase = [];
+const mockPost1 = {
+    ItemID: "e7b1998e-77d3-4cad-9955-f20135d840d0 ",
+    Username: "user_1",
+    Text: "Hello world",
+    Score: 50,
+    Title: "Title"
+};
+const mockPost2 = {
+    ItemID: "29ee2056-c74e-4537-ac95-6234a2506426 ",
+    Username: "user_2",
+    Text: "This is a great song",
+    Score: 100,
+    Title: "Title"
+};
+
+beforeAll(() => {
+    // Mock postDAO
+    postDAO.sendPost.mockImplementation(async (Username, Text, Score, Title) => {
+        const newPost = { ItemID: uuid.v4(), Username, Text, Score, Title };
+
+        mockDatabase.set(Username, newPost);
+        return {
+            $metadata: {
+                httpStatusCode: 201
+            }
+        };
+    });
+});
+
+beforeEach(() => {
+    // Reset database
+    mockDatabase.clear();
+    mockDatabase.push(mockPost1);
+    mockDatabase.push(mockPost2);
+    postDAO.sendPost.mockClear();
+});
 
 describe('createPost test', () => {
-    it('Create post by Sam on Metallica with a 69 score', async () => {
-        const mockPost = jest.mocked(sendPost);
-        const response = await createPost("Sam", "These guys are passable", 69, "Metallica");
-        expect(mockPost).toHaveBeenCalledWith("Same", "These guys are passable", 69, "Metallica");
+    it('Successful post creation', async () => {
+        const username = "Same";
+        const text = "Decent song";
+        const score = 69;
+        const title = "Hello";
+
+        const response = await createPost(username, text, score, title);
+        let added = false;
+        mockDatabase.forEach((post) => {
+            if (post.Username == username && post.Text == text && post.Score == score, post.Title == title) {
+                added = true;
+            }
+        });
+        expect(added).toBeTruthy();
         expect(response).toEqual({message: "Post created successfully"});
     });
-    it('Create post by Jack on Queen with a -1 score', async () => {
+    it('Score too low', async () => {
         const response = await createPost("Jack", "AWFUL!", -1, "Queen");
         expect(response).toEqual({message: "Invalid post"});
     });
-    it('Create post by Tom on Rolling Stones with a 200 score', async () => {
+    it('Score too high', async () => {
         const response = await createPost("Tom", "AWESOME!", 200, "Rolling Stones");
         expect(response).toEqual({message: "Invalid post"});
     });
-    it('Create post by Todd on nothing and saying nothing with a 50 score', async () => {
+    it('No text body', async () => {
         const response = await createPost("Todd", "", 50, "");
         expect(response).toEqual({message: "Invalid post"});
     });
