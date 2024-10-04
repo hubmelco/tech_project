31481ea81ce23d257a34cd8da14bef600c48dc97
camrcast <camrcast@gmail.com>
2024-10-04 09:02:00 -0700

    renamed authentication to middleware and changed post router

--- src/controller/postRouter.js
+++ src/controller/postRouter.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const { createPost } = require('../services/postService');
 const { handleServiceError } = require('../utilities/routerUtilities');
-const { authenticateToken } = require('../middleware/authMiddleware');
+const jwt = require("jsonwebtoken");
+const { authenticate } = require("../middleware/authMiddleware");
 const { validateTextBody, validateScore } = require('../middleware/postMiddleware');
 
 
 const postRouter = express.Router();
 
-postRouter.post("/", authenticateToken, validateTextBody, validateScore, async (req, res) => {
+postRouter.post("/", authenticate, validateTextBody, validateScore, async (req, res) => {
     //TODO check song title exists in API
+    const token = req.headers?.authorization && req.headers.authorization.split(" ")[1];
+    const username = jwt.verify(token, process.env.JWT_SECRET).username;
     try {
-        await createPost(req.User.Username, req.body.Text, req.body.Score, req.body.Title);
-        res.status(201).json({
+        await createPost(username, req.body.text, req.body.score, req.body.title);
+        res.status(200).json({
             messge: "Post successfully created"
         });
     } catch (err) {
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,33 +1,35 @@
 const jwt = require("jsonwebtoken");
-const { decodeJWT, findToken } = require('../utilities/jwtUtilities');
 
-async function authenticateToken(req, res, next){
-    const token = findToken(req);
-    if (!token){
-        res.status(401).json({message: "You must sign in first"});
-        return;
+
+const authenticate = (req, res, next) => {
+    const token = req.headers?.authorization && req.headers.authorization.split(" ")[1];
+    if (!token) {
+        return res.status(401).json("Unauthorized Access");
     }
-    const user = decodeJWT(token);
-    req.User = user;
-    next();
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        res.locals.user = user;
+        next();
+    } catch (err) {
+        return res.status(401).json("Unauthorized Access, try relogging");
+    }
 }
 
-async function authenticateAdminToken(req, res, next){
-    const token = findToken(req);
-    if (!token){
-        res.status(401).json({message: "You must sign in first"});
-        return;
+const adminAuthenticate = (req, res, next) => {
+    const token = req.headers?.authorization && req.headers.authorization.split(" ")[1];
+    if (!token) {
+        return res.status(401).json("Unauthorized Access");
     }
-    const user = decodeJWT(token);
-    if (!user || user.Role !== "Admin"){
-        res.status(403).json({message: "Admin only access"});
-        return;
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        if (user.role !== "admin") {
+            return res.status(401).json({message: "Privilege too low"});
+        }
+        res.locals.user = user;
+        next();
+    } catch (err) {
+        return res.status(401).json("Unauthorized Access, try relogging");
     }
-    req.User = user;
-    next();
 }
 
-module.exports = {
-    authenticateToken,
-    authenticateAdminToken
-};
+module.exports = {authenticate, adminAuthenticate};
