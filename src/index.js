91205628572157dc228f83e9966000814f92e3f4
Jonathan Her <jhawj1@hotmail.com>
2024-10-06 18:25:07 -0500

    Clean up index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ const { userRouter } = require("./controller/userRouter");
 const { postRouter } = require("./controller/postRouter");
 require("dotenv").config();
 
-//const exampleRouter = require("./controller/example")
-
 const app = express();
 const PORT = 3000;
 
@@ -19,4 +17,4 @@ app.use("/users", userRouter);
 app.use("/posts", postRouter);
 
 
-app.listen(PORT, () => console.log("Server listening on http://localhost:3000"));
+app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
