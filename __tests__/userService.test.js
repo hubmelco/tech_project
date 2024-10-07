03fd7929488b87212f7f2743f4d9980a540fd0a6
kylefwalters <41218512+kylefwalters@users.noreply.github.com>
2024-10-07 01:36:16 -0400

    updateRole unit tests

--- __tests__/userService.test.js
+++ __tests__/userService.test.js
@@ -1,7 +1,7 @@
 const uuid = require('uuid');
 const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
-const { register, login } = require('../src/services/userService');
+const { register, login, updateRole } = require('../src/services/userService');
 const userDAO = require('../src/repository/userDAO');
 
 jest.mock('bcrypt');
@@ -10,19 +10,28 @@ jest.mock('../src/repository/userDAO');
 
 const mockDatabase = new Map();
 const mockUser1 = {
-    ItemID: "f162b963-6b4e-4033-9159-2e0c13d78419",
-    Username: "user_1",
-    Password: "password1"
+    itemID: "f162b963-6b4e-4033-9159-2e0c13d78419",
+    username: "user_1",
+    password: "password1",
+    role: "user"
 };
 const mockUser2 = {
-    ItemID: "2dde0401-3c39-42ea-8145-f056fae354f7",
-    Username: "user_2",
-    Password: "password1"
+    itemID: "2dde0401-3c39-42ea-8145-f056fae354f7",
+    username: "user_2",
+    password: "password1",
+    role: "user"
 };
 const mockUser3 = {
-    ItemID: "8885755c-c6f9-4c83-bec4-899e334e7a39",
-    Username: "user_3",
-    Password: "password1"
+    itemID: "8885755c-c6f9-4c83-bec4-899e334e7a39",
+    username: "user_3",
+    password: "password1",
+    role: "user"
+};
+const mockAdmin = {
+    itemID: "81aaccf9-8128-49c5-a51c-12841778bf53",
+    username: "admin_1",
+    password: "password1",
+    role: "admin"
 };
 
 beforeAll(() => {
@@ -31,14 +40,14 @@ beforeAll(() => {
     bcrypt.compareSync.mockImplementation((data, encrypted) => data === encrypted);
 
     // Mock userDAO
-    userDAO.putUser.mockImplementation(async (Username, Password) => {
+    userDAO.putUser.mockImplementation(async (username, password) => {
         const newUser = {
-            ItemID: uuid.v4(),
-            Username,
-            Password
+            itemID: uuid.v4(),
+            username,
+            password
         };
 
-        mockDatabase.set(Username, newUser);
+        mockDatabase.set(username, newUser);
         return {
             $metadata: {
                 httpStatusCode: 200
@@ -46,10 +55,10 @@ beforeAll(() => {
         };
     });
 
-    userDAO.queryByUsername.mockImplementation((Username) => {
+    userDAO.queryByUsername.mockImplementation((username) => {
         let foundUser;
         mockDatabase.forEach((user) => {
-            if (user.Username == Username) {
+            if (user.username == username) {
                 foundUser = user;
             }
         });
@@ -62,18 +71,48 @@ beforeAll(() => {
                 foundUser
             ],
             Count: foundUser ? 1 : 0
-        }
+        };
+    });
+
+    userDAO.getUserById.mockImplementation((id) => {
+        let foundUser;
+        mockDatabase.forEach((user) => {
+            if (user.itemID == id) {
+                foundUser = user;
+            }
+        });
+
+        return {
+            $metadata: {
+                httpStatusCode: 200
+            },
+            Item: foundUser,
+            Count: foundUser ? 1 : 0
+        };
+    });
+
+    userDAO.updateRole.mockImplementation((id, role) => {
+        const user = userDAO.getUserById(id).Item;
+        user.role = role;
+        mockDatabase.set(user.username, user);
+        return {
+            $metadata: {
+                httpStatusCode: 200
+            }
+        };;
     });
 });
 
 beforeEach(() => {
     // Reset database
     mockDatabase.clear();
-    mockDatabase.set(mockUser1.Username, mockUser1);
-    mockDatabase.set(mockUser2.Username, mockUser2);
-    mockDatabase.set(mockUser3.Username, mockUser3);
+    mockDatabase.set(mockUser1.username, mockUser1);
+    mockDatabase.set(mockUser2.username, mockUser2);
+    mockDatabase.set(mockUser3.username, mockUser3);
+    mockDatabase.set(mockAdmin.username, mockAdmin);
     userDAO.putUser.mockClear();
     userDAO.queryByUsername.mockClear();
+    userDAO.getUserById.mockClear();
 });
 
 describe("register", () => {
@@ -85,7 +124,7 @@ describe("register", () => {
         await register(username, password);
         let userAdded = true;
         mockDatabase.forEach((user) => {
-            if (user.Username == username) {
+            if (user.username == username) {
                 userAdded = false;
             }
         });
@@ -97,7 +136,7 @@ describe("register", () => {
         let error;
 
         try {
-            await register(mockUser1.Username, mockUser1.Password);
+            await register(mockUser1.username, mockUser1.password);
         } catch (err) {
             error = err;
         }
@@ -110,9 +149,8 @@ describe("login", () => {
     test("Return new jwt if given valid username and password", async () => {
         const existingUser = mockUser1;
 
-        bcrypt.compareSync.mockImplementation((password) => true);
         jwt.sign.mockReturnValue("usertoken");
-        const token = await login(existingUser.Username, existingUser.Password);
+        const token = await login(existingUser.username, existingUser.password);
 
         expect(token).toBeDefined();
     });
@@ -152,3 +190,57 @@ describe("Delete User Tests", () => {
         expect(1).toBeTruthy();
     })
 })
+
+describe("Change User Role", () => {
+    test("Promotes valid user to admin", async ()=> {
+        const id = mockUser1.itemID;
+        const role = "admin";
+
+        await updateRole(id, role);
+
+        const user = mockDatabase.get(mockUser1.username);
+        expect(user.role).toEqual(role);
+    });
+    
+    test("Throws when user is not found", async ()=> {
+        const id = "Invalid_id";
+        const role = "admin";
+        let error;
+
+        try {
+            await updateRole(id, role);
+        } catch (err) {
+            error = err;
+        }
+
+        expect(error.status).toEqual(400);
+    });
+    
+    test("Throws if user is already role", async ()=> {
+        const id = mockAdmin.itemID;
+        const role = "admin";
+        let error;
+
+        try {
+            await updateRole(id, role);
+        } catch (err) {
+            error = err;
+        }
+
+        expect(error.status).toEqual(400);
+    });
+    
+    test("Throws if trying to demote admin", async ()=> {
+        const id = mockAdmin.itemID;
+        const role = "user";
+        let error;
+
+        try {
+            await updateRole(id, role);
+        } catch (err) {
+            error = err;
+        }
+
+        expect(error.status).toEqual(400);
+    });
+});
