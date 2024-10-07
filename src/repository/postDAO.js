b10b7e5c0f9117f7e973e36d6e10f711e67b1a20
camrcast <camrcast@gmail.com>
2024-10-07 12:43:39 -0700

    Chose my changes over outdated post ones

--- src/repository/postDAO.js
+++ src/repository/postDAO.js
@@ -1,11 +1,48 @@
-const uuid = require("uuid");
-const { PutCommand } = require("@aws-sdk/lib-dynamodb");
+const { PutCommand, ScanCommand, GetCommand, UpdateCommand } = require("@aws-sdk/lib-dynamodb");
 const { TableName, runCommand } = require('../utilities/dynamoUtilities');
 
-async function sendPost(username, text, score, title){
+const CLASS = "post";
+
+async function sendPost(post){
     const command = new PutCommand({
         TableName: TableName,
-        Item: {class: "post", itemID: uuid.v4(), by: username, desc: text, score, title}
+        Item: post
+    });
+    return await runCommand(command);
+}
+
+async function scanPosts() {
+    const command = new ScanCommand({
+        TableName: TableName,
+        FilterExpression: "#class = :class",
+        ExpressionAttributeNames: {
+            "#class": "class"
+        },
+        ExpressionAttributeValues: {
+            ":class": CLASS
+        }
+    })
+    const response = await runCommand(command);
+    return response;
+}
+
+async function sendReply(reply, id){
+    const command = new UpdateCommand({
+        TableName: TableName,
+        Key: {"class": CLASS, "itemID": id},
+        ExpressionAttributeValues: {
+            ":reply": reply
+        },
+        UpdateExpression: "SET replies = list_append(replies, :reply)",
+        ReturnValues: "UPDATED_NEW"
+    });
+    return await runCommand(command);
+}
+
+async function getPost(id) {
+    const command = new GetCommand({
+        TableName,
+        Key: {class: CLASS, itemID: id}
     });
     return await runCommand(command);
 }
