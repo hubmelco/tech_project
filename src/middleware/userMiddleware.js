fb3ecb5384c791398be3b707db2638e808cf764f
kylefwalters <41218512+kylefwalters@users.noreply.github.com>
2024-10-07 14:41:49 -0400

    Check for valid role type

--- src/middleware/userMiddleware.js
+++ src/middleware/userMiddleware.js
@@ -1,21 +1,27 @@
 const { isValidBodyProperty } = require("../utilities/routerUtilities");
 
+const validRoles = ["user", "admin"];
+
 function validateUsername(req, res, next) {
-    if(isValidBodyProperty(req, res, "username")) {
+    if (isValidBodyProperty(req, res, "username")) {
         next();
     }
 }
 
 function validatePassword(req, res, next) {
-    if(isValidBodyProperty(req, res, "password")) {
+    if (isValidBodyProperty(req, res, "password")) {
         next();
     }
 }
 
 function validateRole(req, res, next) {
-    if(isValidBodyProperty(req, res, "role")) {
+    const role = req.body.role;
+    const isValidRole = validRoles.includes(role);
+    if (isValidRole) {
         next();
+        return;
     }
+    res.status(400).json({ message: `Invalid role ${role}` });
 }
 
 module.exports = {
