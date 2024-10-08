4f223d05f3e42b6fe75c7484b77d435c3fe870e0
camrcast <152932964+camrcast@users.noreply.github.com>
2024-10-08 06:59:33 -0700

    Replies and view all posts basic functionality (#7)
    * Created replies and see all posts
    
    * made suggested changes
    
    * Added error handling
    
    * wrote some tests for replies and corrected tests for creating posts
    
    * changed some of the tests and return values
    
    * changed some attribute names postedBy and description
    
    * Chose my changes over outdated post ones

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,12 +1,33 @@
 const { throwIfError } = require('../utilities/dynamoUtilities');
-const { sendPost } = require("../repository/postDAO");
+const postDAO = require("../repository/postDAO");
+const uuid = require("uuid");
 
 async function createPost(username, text, score, title){
-    const post = await sendPost(username, text, score, title);
-    throwIfError(post);
-    return {message: "Post created successfully"};
+    const post = {class: "post", itemID: uuid.v4(), postedBy: username, description: text, score, title, replies: []};
+    const data = await postDAO.sendPost(post);
+    throwIfError(data);
+    return post;
+}
+
+async function seePosts(){
+    const posts = await postDAO.scanPosts();
+    throwIfError(posts);
+    return posts.Items;
+}
+
+async function createReply(username, text, id){
+    const post = await postDAO.getPost(id);
+    if (!post.Item) {
+        throw {status: 400, message: "That post doesn't exist"};
+    }
+    const reply = [{postedBy: username, description: text}];
+    const data = await postDAO.sendReply(reply, id);
+    throwIfError(data);
+    return reply;
 }
 
 module.exports = {
-    createPost
+    createPost,
+    createReply,
+    seePosts
 };
